cedd7057768a271e80114eceebaac0d61148222f
bruno montebello <brunormontebello@hotmail.com>
2020-09-04 00:08:18 +0100

    Add shortener file + test

---
+++ spec/services/shortener_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Shortener do
+  it "Sortens a given URL to a 7 character lookup code" do
+    url = "https://www.favoritewebsite.com/articles/how-to-cook"
+    shortener = Shortener.new(url)
+    expect(shortener.lookup_code.length).to eq(7)
+  end
+end
