4f4e982279b6b18ce2cdf3a5b6c8dd04e1163cc1
bruno montebello <brunormontebello@hotmail.com>
2020-09-05 14:56:34 +0100

    Add test to Link model

---
+++ spec/models/link_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Link do
+
+  it "always has an original URL and a short code" do
+    link = Link.new(
+      original_url: "https://www.favoritewebsite.com/articles/how-to-cook",
+      short_code: "1234567"
+    )
+    link.save
+  end
+
+  it "always has a lookup code" do
+
+  end
+
+end
